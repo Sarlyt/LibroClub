4a6634fcc72f481bde230f75cefd6c52b19d7c31
Zarlyt <93297301+Zarlyt@users.noreply.github.com>
2023-05-29 01:50:26 -0600

    actualizamos routes

--- routes/api/clubs.js
+++ routes/api/clubs.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const {Club} = require('../../db');
+const { Op } = require("sequelize");
 const {check, validationResult} = require('express-validator');
 const { QueryTypes } = require('sequelize');
 
@@ -27,6 +28,26 @@ router.put('/:id', async(req, res)=>{
     });
     res.send('Registro Actualizado');
 });
+
+router.get('/misClubs/:id', async(req, res)=>{
+    const clubV = await Club.findAll({where:{ idUsuario: req.params.id}});
+    res.json(clubV);
+});
+
+router.get('/clubsPublicos/:id', async(req, res)=>{
+    const clubV = await Club.findAll();
+    const libros = JSON.parse(JSON.stringify(clubV));
+    var salida=[];
+    for (var i=0; i <libros.length; i++){
+        if(libros[i].idUsuario!=parseInt(req.params.id)){
+            salida.push(libros[i]);
+        }
+    }
+    res.json(salida);
+});
+
+
+
 
 router.get('/:id', async(req, res)=>{
     const clubV = await Club.findOne({where:{ id: req.params.id}});
